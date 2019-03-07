4845003250136d342484e58680041056257c1c2c
Akihide Sato <akihide315@gmail.com>
2019-03-07 05:53:42 +0000

    新規登録処理、確認画面実装

--- app/controllers/toris_controller.rb
+++ app/controllers/toris_controller.rb
@@ -1,4 +1,27 @@
 class TorisController < ApplicationController
   def index
+    @posts = Post.all
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(set_param)
+    if @post.save
+      redirect_to toris_path , notice:'新規登録完了'
+    else
+      render 'confirm'
+    end
+  end
+  
+  def confirm
+    @post = Post.new(set_param)
+  end
+  
+  
+  
+  private
+  
+  def set_param
+    params.require(:post).permit(:content)
   end
 end
